38bbf05c5c1425d092a7ebfc4de19283b8ec9aa3
sinsin <wr1126@naver.com>
2022-04-28 17:58:12 +0900

    fix timer

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-const startBtn = document.getElementById("start");
-const typeWrap = document.getElementById("wrap");
+const startPage = document.getElementById("start");
+const typePage = document.getElementById("form");
 const sentence = document.getElementById("sentence");
 const typeForm = document.getElementById("typing");
 const typeInput = typeForm.querySelector("input");
 const score = document.querySelector("#score > span");
-const time = document.querySelector("#time > span");
+const lastTime = document.querySelector("#time > span");
 const againBtn = document.querySelector("button");
 
 function randomAdvice() {
@@ -14,55 +14,49 @@ function randomAdvice() {
     .then((advice) => (sentence.innerText = advice));
 }
 
-function countDown() {
-  time.innerText = 15;
-
-  let counter = time.innerText;
-  let scoreCounter = score.innerText;
-  let startTimer = setInterval(timer, 1000);
-
-  function timer() {
-    if (counter < 0) {
-      time.innerText = counter;
-      clearInterval(startTimer);
-      reset();
-    } else if (scoreCounter !== score.innerText) {
-      clearInterval(startTimer);
-    } else {
-      time.innerText = counter;
-      counter -= 1;
-    }
-  }
-}
-
 function start() {
-  startBtn.style.display = "none";
-  typeWrap.style.display = "block";
-  typeInput.disabled = false;
-  randomAdvice();
-  countDown();
+  startPage.style.display = "none";
+  typePage.style.display = "block";
+  setTimer();
 }
 
 function submit(event) {
   event.preventDefault();
+  lastTime.innerText = 15;
+  addScore();
+  randomAdvice();
+}
 
+function setTimer() {
+  let handleTimer = setInterval(() => {
+    let timeInNumber = Number(lastTime.innerText);
+    if (timeInNumber === 0) {
+      clearInterval(handleTimer);
+      showResult();
+    } else {
+      timeInNumber -= 1;
+      lastTime.innerText = timeInNumber;
+    }
+  }, 1000);
+}
+
+function addScore() {
   if (sentence.innerText === typeInput.value) {
     let scoreNumber = Number(score.innerText);
     scoreNumber += 1;
     score.innerText = parseInt(scoreNumber);
   }
   typeInput.value = "";
-  countDown();
-  randomAdvice();
 }
 
-function reset() {
+function showResult() {
   alert(`당신의 점수는 ${score.innerText}점입니다.`);
   score.innerText = 0;
-  time.innerText = 15;
+  lastTime.innerText = 15;
   typeInput.disabled = true;
 }
 
+randomAdvice();
 startBtn.addEventListener("click", start);
 typeForm.addEventListener("submit", submit);
 againBtn.addEventListener("click", start);
